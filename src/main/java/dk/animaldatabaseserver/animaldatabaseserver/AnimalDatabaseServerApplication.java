bc8bde362b43296653b511d5e58280161f66fa6c
SantaDrugen <emil0510@hotmail.com>
2023-09-25 11:42:11 +0200

    Creation

---
+++ src/main/java/dk/animaldatabaseserver/animaldatabaseserver/AnimalDatabaseServerApplication.java
@@ -0,0 +1,13 @@
+package dk.animaldatabaseserver.animaldatabaseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalDatabaseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalDatabaseServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/animaldatabaseserver/animaldatabaseserver/AnimalDatabaseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.animaldatabaseserver.animaldatabaseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalDatabaseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
